6b2a8f3190a7a3cccba804f7a6cc68907ec7477d
Sudhan-23082004 <sudhan.r2022cse@sece.ac.in>
2024-06-10 10:14:14 +0530

    chore: Add react-router-dom and react-toastify dependencies, update App.js to include routing and cart functionality

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return  <footer className="py-1 bg-dark">
+        <p className="text-center text-white mt-1 ">&copy; 2024 Magnetar. All rights reserved</p>
+    </footer>
+}
